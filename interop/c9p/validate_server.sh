6850e06d49c8f09133e8d1dc81799c98c89dab37
Paul deGrandis <paul.degrandis@gmail.com>
2015-10-14 09:24:36 -0400

    Activate the write interop tests

--- interop/c9p/validate_server.sh
+++ interop/c9p/validate_server.sh
@@ -12,22 +12,22 @@ echo "Stat a dir: '/interjections'"
 echo
 echo "Reading a synthetic dir: '/interjections'"
 echo "Skipping -- offset on interop-dirreader isn't working per spec"
-#9 9p -a 127.0.0.1:9090 read /interjections
-#9 9p -a 127.0.0.1:9090 ls /interjections
+#9 9p -a 127.0.0.1:9090 read /interjections # This one doesn't respect offset and just spews forever
+#9 9p -a 127.0.0.1:9090 ls /interjections # This one hangs can causes infinite allocation on the server
 
 echo
 echo "Reading a file: '/interjections/hello'"
 9 9p -a 127.0.0.1:9090 read /interjections/hello
 
 echo
-echo
 echo "Writing to a file: '/interjections/hello'"
-echo "Skipping -- I can't seem to get '9p' cli to write correctly"
-#9 9p -a 127.0.0.1:9090 write /interjections/hello hi
+echo "hello" | 9 9p -a 127.0.0.1:9090 write /interjections/hello
 
 echo
 echo "Reading a file after write: '/interjections/hello'"
 9 9p -a 127.0.0.1:9090 read /interjections/hello
+# Reset the server text...
+echo "Hello World" | 9 9p -a 127.0.0.1:9090 write /interjections/hello
 
 echo
 echo
