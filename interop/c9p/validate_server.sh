68859fcfdd7c00d6e8e0d5a737cfc873a17a286d
Paul deGrandis <paul.degrandis@gmail.com>
2015-10-22 16:12:17 -0400

    c9p interop should cover the other operations

--- interop/c9p/validate_server.sh
+++ interop/c9p/validate_server.sh
@@ -1,17 +1,28 @@
 #!/usr/bin/env bash
 
-# Error: Fid mishandling
 echo
 echo "Reading the root dir: '/'"
-9 9p -a 127.0.0.1:9090 ls /
+9 9p -a 127.0.0.1:9090 ls / # CLJ9P is writing the embedded stat bytes wrong
 
 echo
 echo "Stat a dir: '/interjections'"
 9 9p -a 127.0.0.1:9090 stat /interjections
 
 echo
-echo "Reading a synthetic dir: '/interjections'"
-9 9p -a 127.0.0.1:9090 ls /interjections # This one hangs can causes infinite allocation on the server
+echo "Stat a file: '/interjections/hello'"
+9 9p -a 127.0.0.1:9090 stat /interjections/hello
+
+echo
+echo "Stat a directory: '/interjections'"
+9 9p -a 127.0.0.1:9090 stat /interjections
+
+echo
+echo "ls a file: '/interjections/hello'"
+9 9p -a 127.0.0.1:9090 ls /interjections/hello
+
+echo
+echo "ls a synthetic dir: '/interjections'"
+9 9p -a 127.0.0.1:9090 ls /interjections # CLJ9P is writing the embedded stat bytes wrong
 
 echo
 echo "Reading a file: '/interjections/hello'"
