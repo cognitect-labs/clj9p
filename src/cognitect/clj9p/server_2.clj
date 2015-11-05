04721f9462c79ff5b2392daa2ffe9640011ed78b
Michael Nygard <mtnygard@gmail.com>
2015-11-05 15:03:56 -0600

    Design notes should live in their own file.

--- src/cognitect/clj9p/server_2.clj
+++ src/cognitect/clj9p/server_2.clj
@@ -34,55 +34,6 @@
   (get-remote-id [t]
     (.remoteAddress t)))
 
-;; QidMapping m
-;; ->qid :: m -> r -> Qid
-;; qid-> :: m -> Qid -> r
-
-;; Directory
-;; lookup :: String -> Principal
-
-;; Permission p
-;; Principal = User u | Group g | Other
-;; readable? :: Permission -> Principal -> Bool
-;; writable? :: Permission -> Principal -> Bool
-;; executable? :: Permission -> Principal -> Bool
-;; user :: Permission -> User
-;; group :: Permission -> Group
-;; data operation = Read | Write | Execute
-;; attempt :: (Permissioned r, Operation op) => r -> Principal -> op -> ( r -> x ) -> x | Error
-
-;; Hierarchy h
-;; root :: h -> e
-;; child :: h -> e -> ( e -> Bool ) -> e
-;; parent :: h -> e -> Maybe e
-;; children :: h -> e -> [e]
-
-;; class Permissioned r where
-;;    permission :: r -> Permission
-
-;; Filesystem fs
-;; Resource r (type tbd by the fs)
-;; random-access? :: r -> Bool
-;; exclusive? :: r -> Bool
-;; directory? :: r -> Bool
-;; size :: r -> Long
-;; modified :: r -> Instant
-;; created :: r -> Instant
-;; permission :: r -> Permission
-;; read  :: r -> int -> buffer
-
-;; change-permission :: FileSystem r -> r -> (FileSystem r, r)
-;; create :: FileSystem r -> String -> (FileSystem r, r)
-;; create-directory :: FileSystem r -> String -> (FileSystem r, r)
-;; write :: FileSystem r -> r -> int -> buffer -> (FileSystem r, int)
-;; truncate :: FileSystem r -> r -> (FileSystem r, r)
-;; delete :: FileSystem r -> r -> (FileSystem r, r)
-
-;; ExclusionRealm er
-;; Exclusive e
-;; lock :: ExclusionRealm -> Exclusive -> (ExclusionRealm, Lock e)
-;; unlock :: Lock e -> ExclusionRealm
-
 (defprotocol Handler
   (dispatch [this request] "Returns a channel that will deliver the result"))
 
