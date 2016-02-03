07117a60bb7109ddef9fc637e0e1bf8b24cf2989
Paul deGrandis <paul.degrandis@gmail.com>
2016-02-03 11:40:17 -0500

    Dependent libraries are programmed against SLF4J

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.374"]
                  [io.netty/netty-all "4.0.34.Final"]
-                 [com.barchart.udt/barchart-udt-bundle "2.3.0"]]
+                 [com.barchart.udt/barchart-udt-bundle "2.3.0"]
+                 [org.slf4j/slf4j-api "1.7.14"]]
   :global-vars {*warn-on-reflection* true
                 *unchecked-math* :warn-on-boxed
                 ;*compiler-options* {:disable-locals-clearing true}
